9531f73322486648bc83a67a21d771c7e3c6b6b1
Abhishek Meena <abhishek.meena@credflow.in>
2024-11-11 19:08:36 +0530

    changes for deplyment

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import FallBack from "../Components/Fallback";
 import Home from "../Pages/Home";
 const SaleOrder = lazy(()=> import("../Pages/SaleOrder"));
-const App = lazy(()=> import("../Components/EditSalesOrderModal/test"));
 
 const Router = () => {
   return (
@@ -11,11 +10,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sale" element={<SaleOrder />} />
-        <Route path="/test" element={<App />} />
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/otp-verification" element={<OtpVerification />} />
-        <Route path="/productDetails" element={<ProductDetailsPage />} />
-        <Route path="/view-details" element={<ViewDetails />} /> */}
       </Routes>
     </Suspense>
   );
